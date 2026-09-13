58af3ffa61543ae77b21ba421c6385c1c34c5ac0
j.joaquin.dev <j.joaquin.dev@gmail.com>
2026-09-14 00:22:09 +0800

    fixed double git

---
+++ backend/src/module/auth/index.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../../db/index"; // your drizzle instance
+import * as schema from "../../db/schema";
+import { bearer } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  basePath: "api/auth",
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+    schema,
+  }),
+  plugins: [bearer()],
+  user: {
+    additionalFields: {
+      role: {
+        input: true,
+        defaultValue: ["buyer"],
+        type: "string[]",
+        required: true,
+        returned: true,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+});
---
+++ backend/src/module/seller/service.ts
@@ -0,0 +1,18 @@
+import { db } from "../../db";
+import { sellers_profile } from "../../db/schema";
+import { sellerModel } from "./model";
+
+export async function addProfileService(userId: string, body: sellerModel) {
+  const [postSellerProfile] = await db
+    .insert(sellers_profile)
+    .values({
+      ...body,
+      userId: userId,
+    })
+    .returning();
+
+  if (!postSellerProfile) {
+    throw new Error("Failed to post seller");
+  }
+  return { user: postSellerProfile };
+}
